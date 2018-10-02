c15e7d4fe56769aae57c36cb909e0c53b8e95994
afw821 <afw821@gmail.com>
2018-10-02 13:11:46 -0400

    I added the employee list

--- unsolved/app.js
+++ unsolved/app.js
@@ -1,16 +1,7 @@
-(function() {
-    $('#about').hide();
-})();
-
-const showHome = function(){
-    $('#homepage').show();
-    $('#about').hide();
-};
-
-const showAbout = function(){
-    $('#homepage').hide();
-    $('#about').show();
-};
-
-$('#home').on('click', showHome);
-$('#about-us').on('click', showAbout);
+const renderList = function(){
+    $('.content').empty();
+    for(let i = 0; i < employeeList.length; i++){
+        $('.content').append(`<div class = 'employee'> ${employeeList[i].name} <br/><br/>${employeeList[i].officeNum} <br/><br/> ${employeeList[i].phoneNum} </div>`);
+    }
+}
+renderList();
