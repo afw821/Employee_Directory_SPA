d4c8fa223bd30508c663a640056143781ec5a91b
afw821 <afw821@gmail.com>
2018-10-04 19:09:02 -0400

    I added to the app.js to make the page more functional

--- unsolved/app.js
+++ unsolved/app.js
@@ -1,8 +1,8 @@
-const renderList = function(){
+const renderList = function () {
     $('.content').empty();
-    for(let i = 0; i < employeeList.length; i++){
+    for (let i = 0; i < employeeList.length; i++) {
         $('.content').append(`<div class = 'employee'> ${employeeList[i].name} <br/><br/>${employeeList[i].officeNum} <br/><br/> ${employeeList[i].phoneNum} </div>`);
-    
+
     }
 }
 renderList();
@@ -12,26 +12,65 @@ renderList();
 
 
 
-const viewStart = function(){
+const viewStart = function () {
     $('.content').removeClass('hide');
     $('.input-bar').addClass('hide');
     $('.verify-input-bar').addClass('hide');
+    $('.no').addClass('hide');
+    $('.yes').addClass('hide');
 }
-$('.link-box').on('click',viewStart);
+$('.link-box').on('click', viewStart);
 
-const showAdd = function(){
+const showAdd = function () {
     $('.input-bar').removeClass('hide');
     $('.content').removeClass('hide');
     $('.verify-input-bar').addClass('hide');
-    
+    $('.no').addClass('hide');
+    $('.yes').addClass('hide');
+
 }
 $('.link-box2').on('click', showAdd);
 
-const showVerify = function(){
+const showVerify = function () {
     $('.content').addClass('hide');
     $('.input-bar').addClass('hide');
     $('.verify-input-bar').removeClass('hide');
+    $('.no').addClass('hide');
+    $('.yes').addClass('hide');
 }
 $('.link-box3').on('click', showVerify);
+
+const nameAdd = function () {
+    const nameVal = $('.fname').val();
+    const officeVal = $('.fname2').val();
+    const phoneVal = $('.fname3').val();
+    employeeList.push({
+        name: nameVal,
+        officeNum: officeVal,
+        phoneNum: phoneVal
+    });
+    renderList();    
+}
+$('.fname4').on('click', nameAdd);
+
+const nameSearch = function(){
+    const nameVar = $('.fname5').val();
+for( let i = 0; i < employeeList.length; i++){
+    if( nameVar == employeeList[i].name){
+        $('.yes').removeClass('hide');
+        $('.no').addClass('hide');
+       return "yes";
+       
+    }else{
+        $('.no').removeClass('hide');
+        $('.yes').addClass('hide');
+        return "no";
+    }
+
+}
+
+}
+
+$('.fname6').on('click', nameSearch);
 
 
